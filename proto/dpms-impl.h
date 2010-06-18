58bce78cdc3acc7bebd42218e30607b03299cd52
Tomas Carnecky <tom@dbservice.com>
2010-06-18 23:17:30 +0200

    Implement rest of the dpms protocol

--- proto/dpms-impl.h
+++ proto/dpms-impl.h
@@ -2,6 +2,8 @@
 #include <proto/dpms.h>
 #include <include/globals.h>
 
+int DPMSSet(ClientPtr client, int level);
+
 static int
 impl_DPMS_GetVersion(ClientPtr client, struct req_DPMS_GetVersion *req, struct rep_DPMS_GetVersion *rep)
 {
@@ -25,6 +27,61 @@ impl_DPMS_GetTimeouts(ClientPtr client, struct req_DPMS_GetTimeouts *req, struct
     rep->standby_timeout = DPMSStandbyTime / MILLI_PER_SECOND;
     rep->suspend_timeout = DPMSSuspendTime / MILLI_PER_SECOND;
     rep->off_timeout = DPMSOffTime / MILLI_PER_SECOND;
+
+    return 0;
+}
+
+static int
+impl_DPMS_SetTimeouts(ClientPtr client, struct req_DPMS_SetTimeouts *req)
+{
+    DPMSStandbyTime = req->standby_timeout * MILLI_PER_SECOND;
+    DPMSSuspendTime = req->suspend_timeout * MILLI_PER_SECOND;
+    DPMSOffTime = req->off_timeout * MILLI_PER_SECOND;
+
+    SetScreenSaverTimer();
+
+    return 0;
+}
+
+
+static int
+impl_DPMS_Enable(ClientPtr client, struct req_DPMS_Enable *req)
+{
+    Bool was_enabled = DPMSEnabled;
+    if (DPMSCapableFlag) {
+        DPMSEnabled = TRUE;
+	if (!was_enabled)
+            SetScreenSaverTimer();
+    }
+
+    return 0;
+}
+
+static int
+impl_DPMS_Disable(ClientPtr client, struct req_DPMS_Disable *req)
+{
+    DPMSSet(client, DPMSModeOn);
+    DPMSEnabled = FALSE;
+
+    return 0;
+}
+
+static int
+impl_DPMS_ForceLevel(ClientPtr client, struct req_DPMS_ForceLevel *req)
+{
+    if (!DPMSEnabled)
+        return BadMatch;
+
+    /* TODO: generate this validation from the xcb protocol description */
+    if (req->power_level != DPMSModeOn &&
+        req->power_level != DPMSModeStandby &&
+        req->power_level != DPMSModeSuspend &&
+	req->power_level != DPMSModeOff) {
+        client->errorValue = req->power_level;
+	return BadValue;
+    }
+
+    DPMSSet(client, req->power_level);
 
     return 0;
 }
